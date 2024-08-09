980f983900315a9ea14444e8bfc2665dfaebfee8
spencerwooo <spencer.wushangbo@gmail.com>
2024-08-09 16:58:14 +0800

    fix loading and error page

--- src/app/channel.tsx
+++ src/app/channel.tsx
@@ -24,7 +24,15 @@ export default function Channel({ name, station }: { name: string; station: stri
   if (isLoading) {
     return (
       <div className="w-[21.375rem] h-[12rem] py-2">
-        <div className="font-bold">{name}</div>
+        <a
+          className="text-lg font-medium underline flex items-center"
+          href={station}
+          target="_blank"
+          rel="noopener"
+        >
+          {name}
+          <span className="icon-[iconoir--open-new-window] w-4 h-4 ml-2"></span>
+        </a>
         <div className="text-sm opacity-60 bg-gray-300 dark:bg-neutral-700 h-4 rounded-md mt-2 animate-pulse"></div>
         <ul className="grid grid-cols-5 mt-2 gap-2 animate-pulse">
           {[...Array(10)].map((_, index) => (
@@ -42,7 +50,28 @@ export default function Channel({ name, station }: { name: string; station: stri
     const errorMessage = error?.message || 'Unknown error.'
     return (
       <div className="w-[21.375rem] h-[12rem] py-2">
-        <div className="font-bold">{name}</div>
+        <a
+          className="text-lg font-medium underline flex items-center"
+          href={station}
+          target="_blank"
+          rel="noopener"
+        >
+          {name}
+          <span className="icon-[iconoir--open-new-window] w-4 h-4 ml-2"></span>
+        </a>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <span className="text-sm opacity-60">{name}</span>
+            <span className="icon-[iconoir--pin-alt] w-3.5 h-3.5 ml-1"></span>
+          </div>
+          <button
+            className={`icon-[iconoir--refresh-double] w-3 h-3 ml-1 ${
+              isValidating && 'animate-spin'
+            }`}
+            onClick={() => mutate()}
+          ></button>
+        </div>
         <div className="text-sm text-red-500 mt-2">Error: {errorMessage}</div>
       </div>
     )
@@ -54,9 +83,9 @@ export default function Channel({ name, station }: { name: string; station: stri
   const statusMap = {
     D: ['未启用', 'border-gray-300'],
     E: ['启动', 'border-green-300'],
-    I: ['空闲', 'border-green-400 bg-green-400/20'],
-    C: ['充电中', 'border-yellow-400'],
-    P: ['启动中', 'border-blue-300 bg-blue-300/20'],
+    I: ['空闲', 'border-emerald-400 bg-emerald-400/20'],
+    C: ['充电中', 'border-amber-400'],
+    P: ['启动中', 'border-blue-300 bg-blue-300/30'],
   }
 
   return (
@@ -86,15 +115,13 @@ export default function Channel({ name, station }: { name: string; station: stri
         {Object.keys(channelMap).map((key) => (
           <li
             key={key}
-            className={`relative border-y-2 p-2 pr-3 ${
-              statusMap[channelMap[key].channelStatus][1]
-            }`}
+            className={`relative border-y-2 p-2 pr-3 ${statusMap[channelMap[key].channelStatus][1]}`}
           >
             {channelMap[key].channelStatus === 'C' && (
               <span
-                className="absolute left-0 bottom-0 right-0 bg-yellow-300/20 -z-10"
+                className="absolute left-0 bottom-0 right-0 bg-amber-300/30 -z-10"
                 style={{
-                  top: (channelMap[key].chargedTime / (9 * 60)) * 100 + '%',
+                  top: (1 - channelMap[key].chargedTime / (9 * 60)) * 100 + '%',
                 }}
               />
             )}
